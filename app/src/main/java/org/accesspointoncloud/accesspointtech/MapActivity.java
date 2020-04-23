397e4323ca9f354796e1ae9fa8f0803f0e035a42
Internet of Spyrus <avlonitiss@yahoo.co.uk>
2020-04-23 09:34:14 +0300

    REST

--- app/src/main/java/org/accesspointoncloud/accesspointtech/MapActivity.java
+++ app/src/main/java/org/accesspointoncloud/accesspointtech/MapActivity.java
@@ -15,6 +15,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,12 +23,27 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
     private GoogleMap mMap;
+
     LocationManager locationManager;
     LocationListener locationListener;
+
     public void centreMapOnLocation(Location location, String title){
 
         LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
@@ -82,47 +98,80 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
         mMap = googleMap;
-        Intent intent = getIntent();
-        // Add a marker in incident location and move the camera
-        LatLng incident1 = new LatLng(38, 23.75);
-        mMap.addMarker(new MarkerOptions().position(incident1).title("Υπουργείο Πληροφορικής"));
-       //  mMap.moveCamera(CameraUpdateFactory.newLatLng(incident1));
+        final Intent intent = getIntent();
+        Query myIncident = db.collection("incidentscollection").whereEqualTo("incidentTechEmailField", user.getEmail());
 
-     if (intent.getIntExtra("Place Number",0) == 0 ){
-
-        // Zoom into users location
-        locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                centreMapOnLocation(location,"Είσαι εδώ");
-            }
+        myIncident.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            private static final String TAG ="tech incident docs" ;
+            GeoPoint geoPoint;
+            String yourMission;
 
             @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId());
+                        geoPoint = (GeoPoint) document.get("incidentGeoLocation");
+                        yourMission = (String) document.get("incidentCustCompany");
+                        Log.v("geoPoint for you mission: ",geoPoint.toString());
+                        Log.v("Customer company :", yourMission);
+                       // Intent intent = getIntent();
+                        // Add a marker in incident location and move the camera
 
+                                            }
+                    LatLng incident1 = new LatLng(geoPoint.getLatitude(), geoPoint.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(incident1).title("your mission: "+yourMission));
+
+
+                }
             }
+        });
 
-            @Override
-            public void onProviderEnabled(String s) {
 
+       // if (intent.getIntExtra("Place Number",0) == 0 ){
+
+            // Zoom into users location
+
+            locationManager = (LocationManager)org.accesspointoncloud.accesspointtech.MapActivity.this.getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    centreMapOnLocation(location,"Είσαι εδώ");
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            };
+
+            if (ContextCompat.checkSelfPermission(org.accesspointoncloud.accesspointtech.MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+               // assert lastKnownLocation != null;
+                if (lastKnownLocation != null){centreMapOnLocation(lastKnownLocation,"Your Location");}
+
+            } else {
+
+                ActivityCompat.requestPermissions(org.accesspointoncloud.accesspointtech.MapActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
             }
+     //   }
 
-            @Override
-            public void onProviderDisabled(String s) {
 
-            }
-        };
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
-            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            centreMapOnLocation(lastKnownLocation,"Your Location");
-            mMap.addMarker(new MarkerOptions().position(incident1).title("Υπουργείο Πληροφορικής"));
-        } else {
-
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-        }
-    }}
+}
 }
